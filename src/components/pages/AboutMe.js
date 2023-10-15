d3ecc58fa490cae8b9a48df6c882097431b4033b
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-10-15 11:10:40 -0700

    added new bio

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -18,13 +18,14 @@ export default function AboutMe() {
             </div>
             <h3>About Me</h3>
             <p>
-                Hello! My name is Josh Ungurait. I currently live in downtown Seattle
-                with my dog June. I've been in Washington State for 3 years now after graduating from
-                college in Nashville, TN. I have a degree in photography and love going to see live shows.
-                For the last 3 years I've been working for a fly fishing rod manufacturor on Bainbridge Island. 
-                I love the outdoors and it has been a very rewarding process to learn the ins and outs of 
-                our high detail process. I've been learing how to fly fish as well. I have been web coding for the 
-                last 6 months and have learned a lot. 
+                Hello! My name is Josh Ungurait. I currently live in Seattle, WA with my dog June. I graduated college in 
+                Nashville, TN with a Bachelor's of Fine Art in Photography. For the last 3 years I've been working for a fly fishing rod manufacturor 
+                on Bainbridge Island, WA. I love the outdoors and it has been a very rewarding process to learn the ins and outs of 
+                our high detail process. I started coding in high school leaning C++ and added to my skills while getting my
+                Bachelor's degree in Photography. During that time I leaned HTML and CSS with the goal of designing my own professional 
+                website. I recently completed a Full Stack Web Developer program at the University of Washington. I had an absolute blast 
+                in the program adding greatly to my skills in web development. It was a very rewarding process that contiuously challenged 
+                me to learn and adapt to the ever changing demands of a growing technical field. 
             </p>
            
         </div>
